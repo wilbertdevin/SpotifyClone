42fe96e10c8543354aaed97465f2f6c43490d574
wilbertdevin <wilbertdevin888@gmail.com>
2023-04-06 15:21:20 +0700

    Add Collection View

---
+++ SpotifyClone/Views/Browse/FeaturedPlaylistCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FeaturedPlaylistCollectionViewCell.swift
+//  SpotifyClone
+//
+//  Created by Wilbert Devin Wijaya on 06/04/23.
+//
+
+import UIKit
+
+class FeaturedPlaylistCollectionViewCell: UICollectionViewCell {
+    static let identifier = "FeaturedPlaylistCollectionViewCell"
+}
---
+++ SpotifyClone/Views/Browse/RecommendedTrackCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RecommendedTrackCollectionViewCell.swift
+//  SpotifyClone
+//
+//  Created by Wilbert Devin Wijaya on 06/04/23.
+//
+
+import UIKit
+
+class RecommendedTrackCollectionViewCell: UICollectionViewCell {
+    static let identifier = "RecommendedTrack"
+}
